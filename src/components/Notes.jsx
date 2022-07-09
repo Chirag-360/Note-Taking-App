f92de192589303747674731c4a3e5fd9f808d546
Chirag-360 <107528552+Chirag-360@users.noreply.github.com>
2022-07-09 13:57:52 +0530

    Create Notes.jsx

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>React Note Making Web Application</h1>
+      <p>
+        This web applicaition is meade by using React Framework which is
+        developed by Facebook
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
